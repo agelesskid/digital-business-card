96fa7cd7a4952e2592af857d9341837875abe226
ageless <agelesskidxd@gmail.com>
2023-02-22 21:24:26 +0100

    reworked styles form index.html

--- src/Info.jsx
+++ src/Info.jsx
@@ -5,13 +5,15 @@ export default function ProfilePicture() {
         <div className="info">
             <img src={profilePic} alt="Profile picture" className="profile-pic"></img>
             <div className="info-wrapper">
-                <h2>Laura Smith</h2>
-                <p>Frontend Developer</p>
-                <a href="#">laurasmith.website</a>
-            </div>
-            <div className="links-wrapper">
-                <a href="mailto:#" id="email"><i className="fa-solid fa-envelope"></i>Email</a>
-                <a href="#" id="linkedin"><i className="fa-brands fa-linkedin inverse"></i>Linkedin</a>
+                <div className="name-wrapper">
+                    <h2>Laura Smith</h2>
+                    <p>Frontend Developer</p>
+                    <a href="#">laurasmith.website</a>
+                </div>
+                <div className="links-wrapper">
+                    <a href="mailto:#" id="email"><i className="fa-solid fa-envelope"></i>Email</a>
+                    <a href="#" id="linkedin"><i className="fa-brands fa-linkedin inverse"></i>Linkedin</a>
+                </div>
             </div>
         </div>
     )
